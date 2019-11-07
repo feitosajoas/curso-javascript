44dfd15d1ba46ec4e7c57dd035ba2f5d51ddc100
Joas Feitosa <joasfeitosadelimasilva@gmail.com>
2019-11-07 12:04:00 -0300

    Comentários em JavaScript

---
+++ JavaScript Fundamentos/comentarios.js
@@ -0,0 +1,15 @@
+// comentário de uma linha
+console.log('Linha 1')
+
+/*
+ comentário de
+ múltiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+ * Coméntário de 
+ * múltiplas linhas 
+ */
+console.log*('Linha 3')
+
