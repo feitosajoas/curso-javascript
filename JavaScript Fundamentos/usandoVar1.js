1c550e459a5f9205955d9259db933599ccfdde95
Joas Feitosa <joasfeitosadelimasilva@gmail.com>
2019-11-11 01:54:25 -0300

    Usando Var #1

---
+++ JavaScript Fundamentos/usandoVar1.js
@@ -0,0 +1,19 @@
+{ 
+    { 
+        { 
+            { 
+                var sera = 'Será???'
+                console.log(sera) 
+            } 
+        } 
+    } 
+}
+console.log(sera)
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+console.log(local) // Erro, pois uma váriavél que foi declarada dentro da função, não pode ser chamada fora dela.
