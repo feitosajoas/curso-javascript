9b75a4f5cd22536759de900592fd7f16808a7c15
Joas Feitosa <joasfeitosadelimasilva@gmail.com>
2019-11-12 23:51:30 -0300

    Usando estrutura de controle IF #02

---
+++ Controle/if02.js
@@ -0,0 +1,18 @@
+function testeNum(num) {
+    if (num > 7) {
+        console.log(num)
+        console.log('Final')
+    }
+}
+
+testeNum(6)
+testeNum(8)
+
+function teste2(num) {
+    if(num > 7); { // cuidado com o ;, não usar com as esruturas de controle
+        console.log(num)
+    }
+}
+
+teste2(6)
+teste2(8)
